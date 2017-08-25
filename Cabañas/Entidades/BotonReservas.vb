fba244f0b7dafc177ad0be0d57f19e722845a260
RubenBenegas <ruben_150_@live.com.ar>
2017-08-25 20:00:48 -0300

    clase boton reserva

---
+++ Cabañas/Entidades/BotonReservas.vb
@@ -0,0 +1,17 @@
+﻿Public Class BotonReservas
+    Inherits Button
+    Private id_ As Integer
+
+    Public Property id() As Integer
+        Get
+            Return id_
+        End Get
+        Set(ByVal value As Integer)
+            id_ = value
+        End Set
+    End Property
+
+    Private Sub Reservas_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Me.Click
+        MessageBox.Show("Esta es la reserva: " + Str(id))
+    End Sub
+End Class
